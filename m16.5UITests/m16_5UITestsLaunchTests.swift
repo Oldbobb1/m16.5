d51336a333e45581e948037822963560e75b9a0e
Bobbi R <bobbi@Noutbuk-Bobbi.local>
2023-10-31 19:33:36 +0300

    first commit

---
+++ m16.5UITests/m16_5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  m16_5UITestsLaunchTests.swift
+//  m16.5UITests
+//
+//  Created by Bobbi R. on 10/27/23.
+//
+
+import XCTest
+
+final class m16_5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
